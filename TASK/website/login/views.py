bbf5492a66927ef585444e04a9a5755f9365d39a
VIJAY <151545045+vijaysoftwaredeveloper@users.noreply.github.com>
2024-09-06 19:12:27 +0530

    project

---
+++ TASK/website/login/views.py
@@ -0,0 +1,33 @@
+from django.shortcuts import render
+import mysql.connector as sql
+em=''
+pwd=''
+
+
+
+# Create your views here.
+def loginaction(request):
+    global em,pwd
+    if request.method=='POST':
+        m=sql.connect(host="localhost",user="root",password="vijay",database='website')
+        cursor=m.cursor()
+        d=request.POST
+        for key,value in d.items():
+            if key=="email":
+                em=value
+            if key=="password":
+                pwd=value
+        c="select * from users where email='{}' and password ='{}'".format(em,pwd)
+        cursor.execute(c)
+        t=tuple(cursor.fetchall())
+        if t==():
+            return render(request,'error.html')
+
+        else:
+            return render(request,'wellcome.html')        
+        
+        
+
+    return render(request,'login_page.html')
+        
+    
